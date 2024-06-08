1601afd95a8607b97a7cb1aaf9adf9c526ffc1ea
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2024-06-08 18:30:38 +0300

    fixsed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 1 завдання
 const field1 = prompt("Введіть значення для першого поля:");
 const field2 = prompt("Введіть значення для другого поля:");
 
@@ -7,7 +8,7 @@ if (field1 && field2) {
     alert("Не всі поля заповнені");
 }
 
-// 2
+// 2 завдання
 
 const number1 = prompt("Введіть перше число:");
 const number2 = prompt("Введіть друге число:");
@@ -21,34 +22,34 @@ if (num1 + num2 > 10) {
 }
 
 
-// 3
+// 3 завдання
 
-const text1 = prompt("Введіть текст: ")
+const text = prompt("Введіть текст: ")
 
-if (text1 === "JavaScript") {
+if (text === "JavaScript") {
     alert("Текст містить слово JavaScript");
 } else {
     alert("Текст не містить слово JavaScript");
 }
 
-// 4
+// 4 завдання
 
-const text2 = prompt("Введіть число: ");
+const text1 = prompt("Введіть число: ");
 
-if (text2 >= 10 && text2 <= 20) {
+if (text1 >= 10 && text1 <= 20) {
     alert("Число входить в діапазон від 10 до 20");
 } else {
     alert("Число не входить в діапазон від 10 до 20");
 }
 
-// 5
+// 5 завдання
 
 const name = prompt("Введіть ім`я");
 const email = prompt("Введіть пошту");
 const password = prompt("Введіть пароль");
 
 if (name.length >= 3 && email.includes("@") && password.length >= 6) {
-    window.location.href = "https://edu.goiteens.com/";
+    window.location.href = "https://www.twitch.tv";
 } else {
     alert("Помилка!");
 }
